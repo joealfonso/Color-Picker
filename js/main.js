a05998e6a0e0a948ec95b5e85d8961bd3618693a
switchmotiv <j.alfonso84@gmail.com>
2014-11-18 20:40:22 -0600

    added random color on click event

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    $("body").click(funtion(){
-    	
-    });
+// $(document).ready(function() {
+//     $("body").click(funtion(){
+
+//     });
+// });
+// var red = Math.floor(Math.random()*256);
+// var green = Math.floor(Math.random()*256);
+// var blue = Math.floor(Math.random()*256);
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+randomColor = getRandomColor();
+var bodyElement = document.getElementById("coloredBody");
+document.getElementById("coloredBody").style.backgroundColor = "rgb(0,0,0)";
+document.getElementById("colorValue").textContent = "Click or tap to get a random color";
+
+
+
+document.getElementById("coloredBody").addEventListener('click', function(){
+
+	if (randomColor === "#ffffff")
+		{
+			document.getElementById("coloreValue").style.color = "rgb(0,0,0)";
+		}
+	else
+		{
+			randomColor = getRandomColor();
+			document.getElementById("coloredBody").style.backgroundColor = randomColor;
+			document.getElementById("colorValue").textContent = randomColor;
+
+		}
 });
